63d302f5536c56c5c52ca841334758c25b56a243
Brian Yamasaki <brian.yamasaki@gmail.com>
2022-08-05 22:06:44 -0700

    Update artwork metadata

--- src/components/art-list.js
+++ src/components/art-list.js
@@ -2,28 +2,37 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import * as dayjs from 'dayjs';
 import './layout.css'
 import './art-list.css'
 
 const reverseClass = (i) => {
-  if (i % 2 === 0) return 'flex-reverse';
+  if (i % 2 === 1) return 'flex-reverse';
   return ''
 }
 
 const ArtList = () => {
   const data = useStaticQuery(query);
-  console.log('in PaintingCarousel')
   return (
     <ul className="art-list">
       {
         data.allMdx.nodes.map((node, i) => {
           const image = getImage(node.frontmatter.painting_image);
+          const { title, dimensions, date } = node.frontmatter;
+          const jsDate = dayjs(date);
           return (
             <li key={node.id}>
               <div className={`flex-container ${reverseClass(i)}`}>
-                <GatsbyImage image={image} className="framed-artwork"/>
-                <div className='text-white art-description'>
-                  <MDXRenderer>{node.body}</MDXRenderer>
+                <div className="art-side">
+                  <GatsbyImage image={image} className="framed-artwork"/>
+                </div>
+                <div className='description-side'>
+                  <div className='text-white art-description'>
+                    <h2>{title}</h2>
+                    <p>{jsDate.format('MMMM D, YYYY')}</p>
+                    <p>{dimensions}</p>
+                    <MDXRenderer>{node.body}</MDXRenderer>
+                  </div>
                 </div>
               </div>
             </li>
@@ -43,6 +52,7 @@ export const query = graphql`
           is_landing
           date
           title
+          dimensions
           painting_image {
             childImageSharp {
               gatsbyImageData
