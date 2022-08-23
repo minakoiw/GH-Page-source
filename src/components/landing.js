52c69f60be2bbd8ecc55fcb59e585ee6ebea90fb
Brian Yamasaki <brian.yamasaki@gmail.com>
2022-08-23 16:25:44 -0700

    Add selfie to home page

--- src/components/landing.js
+++ src/components/landing.js
@@ -4,8 +4,16 @@ import {
   Row,
   Col
 } from 'react-bootstrap'
+import { StaticImage } from 'gatsby-plugin-image';
 import LandingPiece from './landing-piece'
 import "./layout.css"
+
+const selfieCss = {
+  height: '85px',
+  width: '120px', 
+  margin: "0 2.5em", 
+  objectFit: 'cover'
+}
 
 const Landing = () => {
   const dataMessage = useStaticQuery(graphql`
@@ -19,13 +27,20 @@ const Landing = () => {
   `);
 
   return (
-    <Row className="landing-zone">
-      <Col lg={{ span:8, offset: 2}}>
-        <a id="landing"></a>
-        <p className="quote">&ldquo;{dataMessage.site.siteMetadata.landingMessage}&rdquo;</p>
-      </Col>
-      <LandingPiece className="landing-piece" />
-    </Row>
+    <>
+      <Row>
+        <Col lg={{ span: 1}}>
+          <StaticImage src="../images/selfie.jpg" alt="selfie" style={selfieCss} />
+        </Col>
+      </Row>
+      <Row className="landing-zone">
+        <Col lg={{ span:8, offset: 2}}>
+          <a id="landing"></a>
+          <p className="quote">&ldquo;{dataMessage.site.siteMetadata.landingMessage}&rdquo;</p>
+        </Col>
+        <LandingPiece className="landing-piece" />
+      </Row>
+    </>
   )
 }
 
